90e7c1449577f6252ce779c6f803f5d04f174fb6
Maxime Baude <contact.mbaude@gmail.com>
2024-09-11 14:20:35 +0200

    finish

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -3,14 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class Home extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function homePage() : Response
+    public function homePage(): Response
     {
         return $this->render('index.html.twig');
+    }
+
+    #[Route('/real', name: 'real')]
+    public function realisationPage(): Response
+    {
+        return $this->render('real.html.twig');
+    }
+
+    #[Route('/devis', name: 'devis')]
+    public function devisPage(): Response
+    {
+        return $this->render('devis.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contactPage(): Response
+    {
+        return $this->render('contact.html.twig');
     }
 }
